bba1861354e0cdd406cf96fcc378981cf90e3075
Sona <125960083+sonaanweb@users.noreply.github.com>
2023-10-05 23:10:03 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -2,9 +2,10 @@
 -- 보호소에 들어올 당시에는 중성화 되지 않았지만, 보호소를 나갈 당시에는 중성화된 동물의 아이디와 생물 종, 이름을 조회하는 아이디 순으로 조회하는 SQL 문을 작성
 SELECT i.animal_id, i.animal_type, i.name
 FROM animal_ins i
-JOIN animal_outs o
+INNER JOIN animal_outs o
 USING (animal_id)
-WHERE i.SEX_UPON_INTAKE != o.SEX_UPON_OUTCOME
+WHERE i.sex_upon_intake LIKE CONCAT("intact","%")
+AND o.sex_upon_outcome NOT LIKE CONCAT("intact","%")
 ORDER BY animal_id
 
 -- intake , outcome 상태가 다른 것들로 출력
