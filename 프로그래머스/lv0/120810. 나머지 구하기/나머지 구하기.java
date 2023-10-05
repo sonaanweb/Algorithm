7bd530672d45505e27fd67a82faabe2655b8962a
Sona <125960083+sonaanweb@users.noreply.github.com>
2023-10-05 23:10:53 +0900

    [level 0] Title: 나머지 구하기, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120810. 나머지 구하기/나머지 구하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        // 나머지 연산자 %
+        int answer = num1 % num2;
+            return answer;
+        // 2. return num1 % num2;
+    }
+}
