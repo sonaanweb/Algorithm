27fab83b934cdd5b03f72d70ec75ea08e7545342
Sona <125960083+sonaanweb@users.noreply.github.com>
2023-10-14 12:56:27 +0900

    [level unrated] Title: 최빈값 구하기, Time: 0.05 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,45 +1,28 @@
 class Solution {
     public int solution(int[] array) {
-        // 배열이 매개변수로 주어졌을 때 최빈값 리턴
-        int answer = 0; // 최빈값
-        int arrMax = 0; // 담겨있는 값 중 가장 큰 수
-        
-        //인덱스 범위를 벗어나는 에러를 유의하기 위해 length 사용
-        for(int i = 0; i < array.length; i++){ 
-            if(array[i] > arrMax){
-                arrMax = array[i];
+        int[] count = new int[1000]; // 배열의 값은 0부터 999까지(인덱스)
+
+        int maxCount = 0;
+        int answer = -1;
+
+        for (int num : array) {
+            count[num]++;
+        }
+
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] > maxCount) {
+                maxCount = count[i];
+                answer = i;
             }
         }
-        // 개수 배열 생성
-        int[] count = new int[arrMax + 1];
-        int max = 0;
-        
-        for (int i = 0; i < array.length; i++){
-            count[array[i]]++;
-        }
-        //count[a] == b; a가 b개
-        
-        // 최빈값
-        // 각 수의 최대 횟수를 count 배열에 담는다
-        for (int i = 0; i < count.length; i++){
-            if(count[i] > max){
-                max = count[i];
-                answer = i; // answer = 입력된 수
+
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] == maxCount && i != answer) {
+                // 중복된 최빈값이 존재
+                return -1;
             }
         }
-        
-        int n = 0;
-        
-        // 최빈값 중복 확인
-        for (int i = 0; i<count.length; i++){
-            if(count[i] == max){ //최대값이 존재한다면
-                n++; // +1
-            }
-        }
-        
-        if(n>1)
-            answer = -1;
-        
+
         return answer;
     }
 }
