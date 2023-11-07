102ef85cb8853a883b7d9afefb1510894ab98b30
Sona <125960083+sonaanweb@users.noreply.github.com>
2023-11-07 14:02:46 +0900

    [level unrated] Title: 배열 뒤집기, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,12 +1,11 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length];// 배열 크기(길이)만큼 생성
-        int index = 0; // 인덱스 변수 초기화 - 배열 순서 추적하기 위함
-
-        // 반복을 배열의 끝 인덱스부터 시작 (i-- : 후위 감소 연산자)
-        for (int i = num_list.length - 1; i >= 0; i--) {
-            answer[index] = num_list[i];
-            index++;
+    
+    	// 배열 생성
+        int[] answer = new int[num_list.length];
+        
+        for(int i = 0; i< num_list.length; i++){
+            answer[i] = num_list[num_list.length-i-1];
         }
         return answer;
     }
