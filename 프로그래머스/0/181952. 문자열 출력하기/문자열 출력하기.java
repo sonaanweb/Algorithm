d8bd17464fbeccabb9cded5e8182d2f6fb4355f0
Sona <125960083+sonaanweb@users.noreply.github.com>
2025-01-23 15:49:11 +0900

    [level 0] Title: 문자열 출력하기, Time: 259.21 ms, Memory: 65.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
+
+// 공백 없으므로 next() 사용, 공백 포함 한줄 전체는 nextLine() 사용
