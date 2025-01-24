1dbb9ad0ccedb95ac678fb054b2af4f8ddf363c7
Sona <125960083+sonaanweb@users.noreply.github.com>
2025-01-24 18:54:19 +0900

    [level 0] Title: 문자열 돌리기, Time: 259.36 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/181945. 문자열 돌리기/README.md
+++ 프로그래머스/0/181945. 문자열 돌리기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 돌리기 - 181945 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181945) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181945?language=java) 
 
 ### 성능 요약
 
-메모리: 64.8 MB, 시간: 152.92 ms
+메모리: 74.5 MB, 시간: 259.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 24일 18:48:25
+2025년 01월 24일 18:54:15
 
 ### 문제 설명
 
--- 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -4,10 +4,8 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        char[] a = str.toCharArray();
-        
-        for(int i = 0; i < str.length(); i++){
-            System.out.println(a[i]);
+        for(char x : str.toCharArray()){
+            System.out.println(x);
         }
     }
 }
