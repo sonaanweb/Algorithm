4ff3037de799aa6b74d803854c77212ae1aef606
Sona <125960083+sonaanweb@users.noreply.github.com>
2025-02-07 15:05:23 +0900

    [level 0] Title: 문자열 섞기, Time: 0.14 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,9 +1,10 @@
 class Solution {
     public String solution(String str1, String str2) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
+        
         for(int i=0; i<str1.length(); i++){
-            answer = answer + str1.charAt(i) + str2.charAt(i);
+            answer.append(str1.charAt(i)).append(str2.charAt(i));
         }
-        return answer;
+        return answer.toString();
     }
 }
