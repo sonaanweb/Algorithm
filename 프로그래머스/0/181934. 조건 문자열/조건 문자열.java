37848d51c1d845d709fab18dd0ffc0e18849ec63
Sona <125960083+sonaanweb@users.noreply.github.com>
2025-02-10 20:47:40 +0900

    [level 0] Title: 조건 문자열, Time: 1.97 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -1,13 +1,11 @@
 class Solution {
     public int solution(String ineq, String eq, int n, int m) {
-        if (ineq.equals(">") && eq.equals("=")) {
-            return (n >= m) ? 1 : 0;
-        } else if (ineq.equals("<") && eq.equals("=")) {
-            return (n <= m) ? 1 : 0;
-        } else if (ineq.equals(">") && eq.equals("!")) {
-            return (n > m) ? 1 : 0;
-        } else {
-            return (n < m) ? 1 : 0;
+        switch (ineq + eq) {
+            case ">=": return (n >= m) ? 1 : 0;
+            case "<=": return (n <= m) ? 1 : 0;
+            case ">!": return (n > m) ? 1 : 0;
+            case "<!": return (n < m) ? 1 : 0;
+            default: return 0;
         }
     }
 }
