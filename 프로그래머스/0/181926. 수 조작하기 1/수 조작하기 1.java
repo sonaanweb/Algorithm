2caf25da6d957ecb03b97cc9d603369906f61635
Sona <125960083+sonaanweb@users.noreply.github.com>
2025-02-14 20:24:40 +0900

    [level 0] Title: 수 조작하기 1, Time: 4.71 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = n;
+        for (char ch : control.toCharArray()){
+            switch (ch){
+                case 'w': answer += 1;
+                break;
+                case 's': answer -= 1;
+                break;
+                case 'd': answer += 10;
+                break;
+                case 'a': answer -= 10;
+                break;
+                default:break;
+            }
+        }
+        return answer;
+    }
+}
